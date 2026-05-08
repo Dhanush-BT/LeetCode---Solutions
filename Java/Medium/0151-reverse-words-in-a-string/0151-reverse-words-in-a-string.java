51d5170c4a3911061ccb4462c18fc711d0bbe617
BT Dhanush <81712810+Dhanush-BT@users.noreply.github.com>
2026-05-08 10:04:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- Java/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ Java/Medium/0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,5 +1,12 @@
 class Solution {
     public String reverseWords(String s) {
+
+        String S[] = s.split("\\s+"),string="";
+        for(int i=S.length-1;i>=0;i--)
+        {
+            string+=S[i]+" ";
+        }
+        return string.trim();
         
     }
 }
